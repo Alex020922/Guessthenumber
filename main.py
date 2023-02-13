465e3111b8e8b47bf0bb9699c4f717751e9843be
Alex020922 <101481344+Alex020922@users.noreply.github.com>
2023-02-13 01:13:13 -0500

    This is a test

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-import random
-def play_game(x):
-    random_number = random.randint(1, x)
-    player_guess = 0
-    while player_guess != random_number:
-        player_guess = int(input(f'Guess a number between 1 and {x}: '))
-        print(player_guess)
-
-    play_game(10)
+print("Print")
